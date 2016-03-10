ea046dbbd544d9b934ec8c42123da73892eb64e2
209255 <209255@student.pwr.edu.pl>
2016-03-10 18:47:59 +0100

    add html service

--- JTTT 1.0/Program.cs
+++ JTTT 1.0/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using HtmlAgilityPack;
+using System.Net;
+using System.Text;
 namespace JTTT_1._0
 {
     static class Program
@@ -17,6 +19,9 @@ namespace JTTT_1._0
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            HTMLService ser = new HTMLService("http://demotywatory.pl");
+            ser.GetPageHtml();
+            ser.PrintPageNodes();
         }
     }
 }
