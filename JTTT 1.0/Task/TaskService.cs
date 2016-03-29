9b28a8f29be8b04a49d5e2af74d58bf4ead44e9e
209255 <209255@student.pwr.edu.pl>
2016-03-29 15:37:16 +0200

    Serialize/Deserialize

--- JTTT 1.0/Task/TaskService.cs
+++ JTTT 1.0/Task/TaskService.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace JTTT_1._0
 {
@@ -36,12 +38,26 @@ namespace JTTT_1._0
 
         public void Serialize()
         {
-            
+            Stream stream = File.Open(_fileName, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, _tasksList);
+            stream.Close();
         }
 
-        public void DeSerialize()
+        public void Deserialize()
         {
-            
+            if (!File.Exists(_fileName)) return;
+            Stream stream = File.Open(_fileName, FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            _tasksList.Clear();
+            BindingList<Task> loaded = (BindingList<Task>)formatter.Deserialize(stream);
+
+            foreach (var t in loaded)
+            {
+                _tasksList.Add(t);
+            }
+
+            stream.Close();
         }
     }
 }
